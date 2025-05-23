331c75fc913d030ebf25b7bd8496d504794dabf5
Jonathan de Koning <jj720462@gmail.com>
2025-05-23 11:27:03 -0500

    test test

---
+++ dekodingTemplate.cpp
@@ -0,0 +1,11 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include "utils/print.hpp"
+
+using namespace std;
+
+int main() {
+    print("Hello");
+}
---
+++ tests/AplusB.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include <iostream>
+
+void main() {
+    long long A, B;
+    std::cin >> A >> B;
+    std::cout << A+B << '\n';
+}
+
